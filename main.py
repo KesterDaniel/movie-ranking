ea8414b4f9b0aa47f046a2f321c7f4a6ff6b717c
KesterDaniel <kesterdan17@gmail.com>
2023-04-03 09:14:41 +0100

    defining the database model

--- main.py
+++ main.py
@@ -7,8 +7,26 @@ from wtforms.validators import DataRequired
 import requests
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
+db = SQLAlchemy(app)
+app.app_context().push()
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 bootstrap = Bootstrap5(app)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, unique=False, nullable=False)
+    description = db.Column(db.String(250), unique=True, nullable=False)
+    rating = db.Column(db.Integer, unique=False, nullable=False)
+    ranking = db.Column(db.Integer, unique=True, nullable=False)
+    review = db.Column(db.String(250), unique=True, nullable=False)
+    img_url = db.Column(db.String(250), unique=True, nullable=False)
+
+
+
+
 
 
 @app.route("/")
